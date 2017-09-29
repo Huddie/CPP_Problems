2012252c92a54010d53e769cc48982744be67813
Ehud Adler <easports96@gmail.com>
2017-09-28 20:42:09 -0400

    Added folders

---
+++ Maze_Solver/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  Maze_Solver
+//
+//  Created by Ehud Adler on 9/28/17.
+//  Copyright © 2017 Ehud Adler. All rights reserved.
+//
+
+#include <iostream>
+#include "mazeSolver.hpp"
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int maze[25] =  {
+        
+        0,  1,  1,  1,  1,
+        0,  1,  0,  0,  0,
+        0,  0,  0,  1,  0,
+        1,  1,  0,  1,  0,
+        0,  1,  0,  1,  0
+        
+    };
+    
+    mazeSolver mazeChecker = mazeSolver(maze);
+    
+    if(mazeChecker.BFS_Check(25, 5, 5, 24)){
+        cout<<"\n\nMaze is valid\n"<<endl;
+    }else{
+        cout<<"\nMaze is invalid\n"<<endl;
+    }
+    
+    return 0;
+}
